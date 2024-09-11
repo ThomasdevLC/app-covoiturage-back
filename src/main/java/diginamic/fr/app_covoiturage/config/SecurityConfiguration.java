1fe3b00b3983e548f26a0145cfd315b8364ba2e8
ThomasdevLC <thomas.lecam@gmail.com>
2024-09-11 18:34:50 +0200

    new profiles dev + prod

--- src/main/java/diginamic/fr/app_covoiturage/config/SecurityConfiguration.java
+++ src/main/java/diginamic/fr/app_covoiturage/config/SecurityConfiguration.java
@@ -2,7 +2,6 @@ package diginamic.fr.app_covoiturage.config;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
