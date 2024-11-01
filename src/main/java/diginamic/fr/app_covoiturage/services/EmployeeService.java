e7d5013ce6ef169afd2105d92a5c95b006bdaf30
ThomasdevLC <thomas.lecam@gmail.com>
2024-11-01 16:42:58 +0100

    employee controller + service code  cleaned

--- src/main/java/diginamic/fr/app_covoiturage/services/EmployeeService.java
+++ src/main/java/diginamic/fr/app_covoiturage/services/EmployeeService.java
@@ -1,14 +1,9 @@
 package diginamic.fr.app_covoiturage.services;
 
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import diginamic.fr.app_covoiturage.dto.employee.EmployeeProfileDTO;
-import diginamic.fr.app_covoiturage.dto.employee.EmployeeRegisterDTO;
 import diginamic.fr.app_covoiturage.dto.employee.EmployeeRideSharesDTO;
 import diginamic.fr.app_covoiturage.mapper.employee.EmployeeProfileMapper;
 import diginamic.fr.app_covoiturage.mapper.employee.EmployeeRideSharesMapper;
@@ -21,45 +16,14 @@ public class EmployeeService {
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    @Autowired
-    private EmployeeRideSharesMapper employeeRideSharesMapper;
+    // @Autowired
+    // private EmployeeRideSharesMapper employeeRideSharesMapper;
 
     @Autowired
     private EmployeeProfileMapper employeeProfileMapper;
 
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
-
     public Employee save(Employee employee) {
         return employeeRepository.save(employee);
-    }
-
-    public Employee registerEmployee(EmployeeRegisterDTO employeeRegisterDTO) {
-        Employee employee = new Employee();
-        employee.setFirstName(employeeRegisterDTO.getFirstName());
-        employee.setLastName(employeeRegisterDTO.getLastName());
-        employee.setGender(employeeRegisterDTO.getGender());
-        employee.setPhone(employeeRegisterDTO.getPhone());
-        employee.setEmail(employeeRegisterDTO.getEmail());
-        employee.setPassword(bCryptPasswordEncoder.encode(employeeRegisterDTO.getPassword()));
-        return employeeRepository.save(employee);
-    }
-
-    public Employee updateEmployee(int id, Employee updateEmployee) {
-        Optional<Employee> optionalEmployee = employeeRepository.findById(id);
-        if (optionalEmployee.isPresent()) {
-            Employee employee = optionalEmployee.get();
-            employee.setFirstName(updateEmployee.getFirstName());
-            employee.setLastName(updateEmployee.getLastName());
-            employee.setGender(updateEmployee.getGender());
-            employee.setPhone(updateEmployee.getPhone());
-            employee.setAdmin(updateEmployee.isAdmin());
-            employee.setEmail(updateEmployee.getEmail());
-            employee.setPassword(updateEmployee.getPassword());
-            return employeeRepository.save(employee);
-        } else {
-            throw new RuntimeException("Employee not found with id " + id);
-        }
     }
 
     public Employee deleteEmployeeAccount(int id) {
@@ -69,14 +33,14 @@ public class EmployeeService {
             employee.setActive(false);
             return employeeRepository.save(employee);
         } else {
-            throw new RuntimeException("Employee not found with id " + id);
+            throw new RuntimeException("Utilisateur non reconnu");
         }
     }
 
-    public Optional<EmployeeRideSharesDTO> getEmployeeById(int id) {
-        return employeeRepository.findById(id)
-                .map(employeeRideSharesMapper::toDTO);
-    }
+    // public Optional<EmployeeRideSharesDTO> getEmployeeById(int id) {
+    // return employeeRepository.findById(id)
+    // .map(employeeRideSharesMapper::toDTO);
+    // }
 
     public Optional<EmployeeProfileDTO> getEmployeeProfile(int id) {
         return employeeRepository.findById(id)
