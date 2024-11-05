d45ca6b6e6f4caf11197a2b8c7ef244927b02484
ThomasdevLC <thomas.lecam@gmail.com>
2024-11-05 18:30:18 +0100

    exceptionHandler test

--- src/main/java/diginamic/fr/app_covoiturage/controlleradvice/ExceptionHandlerAdvice.java
+++ src/main/java/diginamic/fr/app_covoiturage/controlleradvice/ExceptionHandlerAdvice.java
@@ -2,7 +2,6 @@ package diginamic.fr.app_covoiturage.controlleradvice;
 
 import java.util.HashMap;
 import java.util.Map;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
