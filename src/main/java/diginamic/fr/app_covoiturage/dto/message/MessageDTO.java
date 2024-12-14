bf7232bc7814fd52a5c61a0696c6ffc77ec2ddac
ThomasdevLC <thomas.lecam@gmail.com>
2024-12-14 15:34:34 +0100

    Add isRead and isDeleted properties to MessageDTO

--- src/main/java/diginamic/fr/app_covoiturage/dto/message/MessageDTO.java
+++ src/main/java/diginamic/fr/app_covoiturage/dto/message/MessageDTO.java
@@ -7,15 +7,20 @@ public class MessageDTO {
     private int id;
     private String content;
     private LocalDateTime date;
+    private boolean isRead;
+    private boolean isDeleted;
     private List<Integer> employeeIds;
 
     public MessageDTO() {
     }
 
-    public MessageDTO(int id, String content, LocalDateTime date, List<Integer> employeeIds) {
+    public MessageDTO(int id, String content, LocalDateTime date, boolean isRead, boolean isDeleted,
+            List<Integer> employeeIds) {
         this.id = id;
         this.content = content;
         this.date = date;
+        this.isRead = isRead;
+        this.isDeleted = isDeleted;
         this.employeeIds = employeeIds;
 
     }
@@ -50,6 +55,22 @@ public class MessageDTO {
 
     public void setEmployeeIds(List<Integer> employeeIds) {
         this.employeeIds = employeeIds;
+    }
+
+    public boolean isRead() {
+        return isRead;
+    }
+
+    public void setRead(boolean isRead) {
+        this.isRead = isRead;
+    }
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(boolean isDeleted) {
+        this.isDeleted = isDeleted;
     }
 
 }
